0cf8b463584c90116f37baf8411b57738fbff793
febriantok29-fork <febriantok29@gmail.com>
2026-03-02 10:12:35 +0700

    Refactor room endpoints to include update and delete methods for improved functionality and consistency

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,10 @@ Route::prefix('v1')->middleware('jwt')->group(function () {
 
     // Room endpoints
     Route::get('/rooms', [RoomController::class, 'index']);
+    Route::post('/rooms', [RoomController::class, 'store']);
     Route::get('/rooms/{id}', [RoomController::class, 'show']);
+    Route::put('/rooms/{id}', [RoomController::class, 'update']);
+    Route::delete('/rooms/{id}', [RoomController::class, 'destroy']);
     Route::get('/rooms/{id}/availability', [RoomController::class, 'availability']);
 
     // Reservation endpoints
